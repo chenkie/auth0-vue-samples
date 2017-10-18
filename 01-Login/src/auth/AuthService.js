be1e655a93915aef45a0179258ee601fd7d66299
Ryan Chenkie <ryanchenkie@gmail.com>
2017-10-18 14:41:20 -0500

    add login start

--- 01-Login/src/auth/AuthService.js
+++ 01-Login/src/auth/AuthService.js
@@ -24,7 +24,7 @@ export default class AuthService {
   })
 
   login () {
-    this.auth0.authorize()
+    // redirect to auth0 to log in
   }
 
   handleAuthentication () {
@@ -45,18 +45,15 @@ export default class AuthService {
     let expiresAt = JSON.stringify(
       authResult.expiresIn * 1000 + new Date().getTime()
     )
-    localStorage.setItem('access_token', authResult.accessToken)
-    localStorage.setItem('id_token', authResult.idToken)
-    localStorage.setItem('expires_at', expiresAt)
+    // set access token, id token, and expiry time in local storage
+
+
     this.authNotifier.emit('authChange', { authenticated: true })
   }
 
   logout () {
     // Clear access token and ID token from local storage
-    localStorage.removeItem('access_token')
-    localStorage.removeItem('id_token')
-    localStorage.removeItem('expires_at')
-    this.userProfile = null
+
     this.authNotifier.emit('authChange', false)
     // navigate to the home route
     router.replace('home')
@@ -65,7 +62,5 @@ export default class AuthService {
   isAuthenticated () {
     // Check whether the current time is past the
     // access token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'))
-    return new Date().getTime() < expiresAt
   }
 }
